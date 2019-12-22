5d4de135d022edc39630cf265593f3aafad7df7a
Yunus Ayar <greybley@yahoo.de>
2019-12-22 20:11:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,55 +1,51 @@
-#include <iostream>
-#include <string>
-#include <limits>
-
 #include "optional.hpp"
 
-inline void hello(optional<std::string> _what)
+#include <iostream>
+#include <limits>
+#include <string>
+
+inline void hello(optional<std::string> what)
 {
-	_what.if_present([](auto x) { std::cout << "Hello, " << x << std::endl; });
+    what.if_present([](const std::string& x) { std::cout << "Hello, " << x << std::endl; });
 }
 
-inline double convert(optional<std::string> _what)
+inline double convert(optional<std::string> what)
 {
-	return _what.map([](auto x) { return std::stod(x); }).or_else(std::numeric_limits<double>::quiet_NaN());
-}
-
-inline optional<std::string> map()
-{
-	return "";
+    return what.map(&std::stod, static_cast<size_t*>(0)).or_else(std::numeric_limits<double>::quiet_NaN());
 }
 
 int main()
 {
+    hello({});
+    hello("World!");
 
-	hello({});
-	hello("World!");
+    std::cout << convert("5.36") << std::endl;
+    std::cout << convert({}) << std::endl;
 
-	std::cout << convert("5.36") << std::endl;
-	std::cout << convert({}) << std::endl;
+    std::cout << "c str: " << optional<std::string>("hi").map(&std::string::c_str).or_else(nullptr) << std::endl;
 
-	auto a = []() {return 3.4; };
+    optional<int> l;
 
+    std::cout << "l or 55: " << l.or_else(55) << std::endl;
+    std::cout << "l or 3*3: " << l.or_else_get([] { return 3 * 3; }) << std::endl;
 
-	optional_detail::function_result<decltype(a)>::type;
+    // hash of empty optional
+    std::cout << "hash of empty optional==0: " << (std::hash<optional<int>>()(l) == std::hash<int>()(0)) << std::endl;
 
+    l.if_present_or_else([](int x) { std::cout << "l is present: " << x << std::endl; },
+                         [&l] {
+                             std::cout << "l is not present; setting it to 61" << std::endl;
+                             l.emplace(61);
+                         });
 
-	try {
-		optional<int> _l;
+    std::cout << "l as double + 10%: " << l.map([](int x) { return x * 1.1; }).get() << std::endl;
 
-		_l.or_else_get([v = 3]() -> const int {
-			return v;
-		});
-		std::cout << (std::hash<optional<int>>()(_l) == std::hash<int>()(0)) << std::endl;
-		_l.if_present([](auto x) { std::cout << x << std::endl; });
+    try {
+        optional<int> x;
 
-		std::cout << _l.or_else(55) << std::endl;
-
-		std::cout << _l.map([](auto x) { return static_cast<double>(x) + 0.6; }).get() << std::endl;
-
-	} catch (const no_such_element_error& e) {
-		std::cout << e.what() << std::endl;
-	}
-
-	system("pause");
+        // getting empty optional
+        x.get();
+    } catch (const no_such_element_error& e) {
+        std::cout << e.what() << std::endl;
+    }
 }
